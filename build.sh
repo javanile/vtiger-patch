f032ddb0eef9be23b06fe99b453442311561363e
francescobianco <info.francescobianco@gmail.com>
2021-12-16 15:09:49 +0100

    First commit

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+rm -fr PATCH_*
+
+[ -d CLONE ] || git clone https://code.vtiger.com/vtiger/vtigercrm.git CLONE
+
+cd CLONE
+
+FROM=master
+while IFS="" read -r p || [ -n "$p" ]; do
+  FILE=../PATCH_${p}_${FROM}
+  git log --pretty=oneline ^${p} ${FROM} | cut -d' ' -f1 > tmp
+  sed -e 's#^#https://code.vtiger.com/vtiger/vtigercrm/commit/#' tmp > "$FILE"
+  rm tmp
+  FROM=$p
+done < ../TAGS
